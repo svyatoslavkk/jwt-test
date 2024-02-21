286dd3bd4b809c71df1c2c9a1da2f8d821f2a074
svyatoslavkk <sv.krivonos1@gmail.com>
2024-02-21 11:38:42 +0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+    colors: {
+      blueLight: '#1395b6',
+      blueOcean: '#0299c8',
+    },
+  },
+  plugins: [],
+} satisfies Config;
